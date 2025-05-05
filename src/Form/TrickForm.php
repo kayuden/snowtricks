83835f3fae1590cc5bc6f91fb3e7cac9f73ac667
kayuden <kbartholomot@gmail.com>
2025-05-05 15:43:05 +0200

    Form creation

---
+++ src/Form/TrickForm.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Trick;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class TrickForm extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('name')
+            ->add('description')
+            ->add('type')
+            ->add('createdAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('editedAt', null, [
+                'widget' => 'single_text',
+            ])
+            ->add('imagePath')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Trick::class,
+        ]);
+    }
+}
